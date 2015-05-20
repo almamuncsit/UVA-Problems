7b31426eb57078bfbdcb3089429631378189f0f5
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-21 00:25:58 +0600

    Create Bishops.cpp

---
+++ Bishops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int t, r1, c1, r2, c2;
+    cin>>t;
+    for (int i = 1; i<=t; i++) {
+        cin>>r1>>c1>>r2>>c2;
+
+    }
+
+    return 0;
+}
